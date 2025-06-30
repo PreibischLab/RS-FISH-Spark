d298dc686bb0898391b58cf9ece70ce4ac6790ec
Cristian Goina <goinac@janelia.hhmi.org>
2025-06-30 18:09:42 -0400

    update git hash in docker image

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -2,4 +2,4 @@
 
 # Run this using `./build-docker-image.sh --load|--push`
 
-docker buildx build --platform linux/arm64,linux/amd64 --tag ghcr.io/janeliascicomp/rs-fish-spark:f92e550 . $*
+docker buildx build --platform linux/arm64,linux/amd64 --tag ghcr.io/janeliascicomp/rs-fish-spark:18ca964 . $*
