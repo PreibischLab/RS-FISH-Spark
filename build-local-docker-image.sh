0a368abffea67c3da66d4d098bae84f1304df621
Cristian Goina <goinac@janelia.hhmi.org>
2025-07-08 17:03:14 -0400

    exclude log4j backends from dependencies

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -2,4 +2,9 @@
 
 # Run this using `./build-docker-image.sh --load|--push`
 
-docker buildx build --platform linux/arm64,linux/amd64 --tag ghcr.io/janeliascicomp/rs-fish-spark:18ca964 . $*
+GIT_HASH=a06db09
+docker buildx build \
+       --platform linux/arm64,linux/amd64 \
+       --tag ghcr.io/janeliascicomp/rs-fish-spark:${GIT_HASH} \
+       --build-arg RS_FISH_SPARK_GIT_HASH=${GIT_HASH} \
+       . $*
