63f5b7b1be2156fa7e8f6bf6bd4f6158594c5b21
Cristian Goina <goinac@janelia.hhmi.org>
2025-09-05 16:36:24 -0400

    updated build docker script

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Run this using `./build-docker-image.sh --load|--push`
+# Run this using `./build-docker-image.sh docker|podman --load|--push`
 
-GIT_HASH=a06db09
-docker buildx build \
+BUILD_CONTAINER_TOOL=$1
+shift
+GIT_HASH=f5cad2d
+
+echo "Build RS-FISH:${GIT_HASH} container using ${BUILD_CONTAINER_TOOL}"
+
+if [[ "${BUILD_CONTAINER_TOOL}" == "podman" ]] ; then
+  podman build \
        --platform linux/arm64,linux/amd64 \
        --tag ghcr.io/janeliascicomp/rs-fish-spark:${GIT_HASH} \
        --build-arg RS_FISH_SPARK_GIT_HASH=${GIT_HASH} \
-       . $*
+       . \
+       $*
+else
+  docker buildx build \
+       --platform linux/arm64,linux/amd64 \
+       --tag ghcr.io/janeliascicomp/rs-fish-spark:${GIT_HASH} \
+       --build-arg RS_FISH_SPARK_GIT_HASH=${GIT_HASH} \
+       . \
+       $*
+fi
