b26d39c497770e79edc4fd0ba222f9b43634b321
Cristian Goina <goinac@janelia.hhmi.org>
2026-06-05 11:05:48 -0400

    upgrade java; change docker build script

--- build-local-docker-image.sh
+++ build-local-docker-image.sh
@@ -3,14 +3,21 @@
 what=$1
 shift
 
-GIT_HASH=e81ca9e
+GIT_HASH=bcbe2c7
 
 echo "Build RS-FISH:${GIT_HASH}"
 
-IMAGE_NAME=ghcr.io/janeliascicomp/rs-fish-spark:omedev-${GIT_HASH}
+IMAGE_NAME=ghcr.io/janeliascicomp/rs-fish-spark:spark4.1.2-scala2.13-java21-ubuntu24.04-zarrv3-omedev-${GIT_HASH}
 
 case $what in
-  --build)
+  --docker-build)
+  docker buildx build \
+        --platform linux/amd64,linux/arm64 \
+        -t $IMAGE_NAME \
+        -f Dockerfile \
+        $*
+  ;;
+  --podman-build)
   # remove existing image
   podman manifest rm ${IMAGE_NAME} -i
   podman image rm ${IMAGE_NAME} -f
@@ -22,7 +29,7 @@ case $what in
         -f Dockerfile \
         $*
   ;;
-  --build-and-push)
+  --podman-build-and-push)
   # remove existing image
   podman manifest rm ${IMAGE_NAME} -i
   podman image rm ${IMAGE_NAME} -f
@@ -36,7 +43,7 @@ case $what in
   echo "Push ${IMAGE_NAME} images"
   podman manifest push ${IMAGE_NAME}
   ;;
-  --push)
+  --podman-push)
   echo "Push ${IMAGE_NAME} images"
   podman manifest push ${IMAGE_NAME}
   ;;
